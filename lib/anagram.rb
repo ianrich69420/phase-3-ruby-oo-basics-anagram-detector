b297fe6190ee938569758844c441e873cf5cff8e
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-22 10:27:13 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        matches = []
+        array.each do |word|
+            if word.downcase.chars.sort == @word.downcase.chars.sort
+                matches.push(word)
+            end
+        end
+        matches
+    end
+end
