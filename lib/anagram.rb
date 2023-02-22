668bee29b47403886565461147d0b5473ca57232
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-22 10:26:05 +0300

    commit tree a85f10cad6c17ee7edfaa21973110631d5df676d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        matches = []
+        array.each do |word|
+            if word.downcase.chars.sort == @word.downcase.chars.sort
+                matches.push(word)
+            end
+        end
+        matches
+    end
+end
+
+word = Anagram.new("Words")
+
+word.match(%w[sword doors cords])
+
+binding.pry
